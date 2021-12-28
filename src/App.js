c8c44671ddcaa20a934ebf94ae33e65d9a94b05d
Gaurav <85581658+Gaurav4code@users.noreply.github.com>
2021-12-29 01:13:26 +0530

    Cta Section Added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles.css";
 import Nav from "../src/components/Nav";
 import { Header } from "../src/components/Header";
 import { Cards } from "./components/Cards";
+import { Cta } from "./components/Cta";
 export default function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
       <Cards />
+      <Cta />
     </div>
   );
 }
---
+++ src/components/Cta.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Cta = () => {
+  return (
+    <Container>
+      <Text>
+        Interested working
+        <br />
+        with me?
+      </Text>
+      <Button>
+        <i class="fas fa-envelope-square" />
+        Email Me
+      </Button>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 224px;
+  width: 1110px;
+  display: flex;
+  justify-content: space-evenly;
+  border-radius: 25px;
+
+  margin-top: 50px;
+
+  margin-left: 165px;
+  margin-right: 165px;
+  background: #eebf63;
+`;
+
+const Text = styled.h1`
+  font-family: Poppins;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 62px;
+  letter-spacing: 0em;
+  text-align: left;
+  height: 124px;
+  width: 457px;
+
+  padding-top: 45px;
+
+  color: white;
+`;
+const Button = styled.button`
+  height: 70px;
+  width: 259px;
+  left: 941px;
+  margin-top: 77px;
+  border: none;
+  border-radius: 10px;
+  font-family: Poppins;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 36px;
+  letter-spacing: 0px;
+  text-align: left;
+  padding: 0 50px;
+  i {
+    font-size: 21px;
+    margin-right: 16px;
+  }
+`;
